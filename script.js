d13f88ad2dd977d21f8c4e84c86930f2e5ee802b
aeoterra <90536114+aeoterra@users.noreply.github.com>
2025-02-05 18:05:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const messages = [
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "Ok.. ok.. fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
 
